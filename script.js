85c232ba6212758dc37cf1526c761c559c0a5cb2
Peter <157177875+petervancleave@users.noreply.github.com>
2025-03-09 00:23:17 -0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", function() {
-    let scrollPosition = window.scrollY * 0.05; // Adjust this value for parallax speed
-    document.getElementById("background").style.transform = `translateY(${scrollPosition}px)`; // Use translateY
-});
